c41e5f5f5aae767e6fae1f5cf474924d44a91386
Inspiration <aston.bush99@gmail.com>
2021-08-28 00:22:34 +0100

    Update ticket.py

--- Packs/ticket.py
+++ Packs/ticket.py
@@ -1,48 +1,113 @@
 @formexe.command(pass_context = True)
 async def ticket(ctx):
+    await ctx.message.delete()
     tauth = ctx.message.author
     msg = await ctx.send('Creating Ticket...')
     await asyncio.sleep(3)
     await msg.delete()
-    category = discord.utils.get(tauth.guild.categories, id=())
-    ibtc = discord.utils.get(tauth.guild.roles, id=())
-    channel = await ctx.message.guild.create_text_channel('Ticket')
-    msg = await channel.send(f'{tauth.mention} Someone will be with you shortly '+ibtc.mention)
-    ibe = await channel.set_permissions(ctx.message.guild.default_role, read_messages=False, send_messages=False)
-    ibc = await channel.set_permissions(tauth, read_messages=True, send_messages=True)
-    iba = await channel.set_permissions(ibtc, read_messages=True, send_messages=True)
+    guild = formexe.get_guild(formemg)
+    logs = discord.utils.get(guild.channels, name='ticket-logs')
+    if logs == None:
+        cat = await guild.create_category(name='Tickets')
+        c = await cat.create_text_channel(name=f'ticket-logs', topic=('DO NOT EDIT THE NAME OF THIS CHANNEL'))
+        await c.send('DO NOT EDIT THE NAME OF THIS CHANNEL, if you want it changed contact support')
+        logs = discord.utils.get(guild.channels, name='ticket-logs')
+    category = logs.category
+    ulist = []
+    for channel in category.channels:
+        if (channel.topic) == None:
+            await channel.edit(topic=formexe.user.id)
+            ulist.append(int(channel.topic))
+        else:
+            try:
+                if int(channel.topic) == (tauth.id):
+                    current = formexe.get_channel(channel.id)
+                    ulist.append(int(channel.topic))
+            except:
+                pass
+    i = (tauth.id)
+    if i not in ulist:
+        channel = await ctx.message.guild.create_text_channel(name=f'{tauth.name} ticket', topic=(tauth.id), category=category)
+        msg = await channel.send(f'{tauth.mention} Someone will be with you shortly')
+        ibc = await channel.set_permissions(tauth, read_messages=True, send_messages=True)
+    else:
+        await current.send(f'{tauth.mention} You already have a ticket')
 
 @formexe.command(pass_context = True)
 async def report(ctx):
+    await ctx.message.delete()
     tauth = ctx.message.author
-    msg = await ctx.send('Creating Ticket...')
+    msg = await ctx.send('Creating Report Ticket...')
     await asyncio.sleep(3)
     await msg.delete()
-    category = discord.utils.get(tauth.guild.categories, id=())
-    ibtc = discord.utils.get(tauth.guild.roles, id=())
-    channel = await ctx.message.guild.create_text_channel('Ticket-Report')
-    msg = await channel.send(f'{tauth.mention} Someone will be with you shortly '+ibtc.mention)
-    ibe = await channel.set_permissions(ctx.message.guild.default_role, read_messages=False, send_messages=False)
-    ibc = await channel.set_permissions(tauth, read_messages=True, send_messages=True)
-    iba = await channel.set_permissions(ibtc, read_messages=True, send_messages=True)
+    guild = formexe.get_guild(formemg)
+    logs = discord.utils.get(guild.channels, name='ticket-logs')
+    if logs == None:
+        cat = await guild.create_category(name='Tickets')
+        c = await cat.create_text_channel(name=f'ticket-logs', topic=('DO NOT EDIT THE NAME OF THIS CHANNEL'))
+        await c.send('DO NOT EDIT THE NAME OF THIS CHANNEL, if you want it changed contact support')
+        logs = discord.utils.get(guild.channels, name='ticket-logs')
+    category = logs.category
+    ulist = []
+    for channel in category.channels:
+        if (channel.topic) == None:
+            await channel.edit(topic=formexe.user.id)
+            ulist.append(int(channel.topic))
+        else:
+            try:
+                if int(channel.topic) == (tauth.id):
+                    current = formexe.get_channel(channel.id)
+                    ulist.append(int(channel.topic))
+            except:
+                pass
+    i = (tauth.id)
+    if i not in ulist:
+        channel = await ctx.message.guild.create_text_channel(name=f'{tauth.name} report', topic=(tauth.id), category=category)
+        msg = await channel.send(f'{tauth.mention} Someone will be with you shortly')
+        msg = await channel.send('If you can send the users name or id and the evidence that would help us out. Thanks')
+        ibc = await channel.set_permissions(tauth, read_messages=True, send_messages=True)
+    else:
+        await current.send(f'{tauth.mention} You already have a ticket')
 
 @formexe.command(pass_context = True)
 async def vcticket(ctx):
+    await ctx.message.delete()
     tauth = ctx.message.author
-    msg = await ctx.send('Creating Ticket...')
+    msg = await ctx.send('Creating Vc Ticket...')
     await asyncio.sleep(3)
     await msg.delete()
-    category = discord.utils.get(tauth.guild.categories, id=())
-    c = await guild.create_voice_channel(name='voice ticket')
-    m = formexe.get_channel((c.id))
+    guild = formexe.get_guild(formemg)
+    logs = discord.utils.get(guild.channels, name='ticket-logs')
+    if logs == None:
+        cat = await guild.create_category(name='Tickets')
+        c = await cat.create_text_channel(name=f'ticket-logs', topic=('DO NOT EDIT THE NAME OF THIS CHANNEL'))
+        await c.send('DO NOT EDIT THE NAME OF THIS CHANNEL, if you want it changed contact support')
+        logs = discord.utils.get(guild.channels, name='ticket-logs')
+    category = logs.category
+    channel = await guild.create_voice_channel(name=f'{tauth.name} ticket', category=category)
     msg = await ctx.send(f'{tauth.mention} Someone will be with you shortly')
-    ibc = await m.set_permissions(tauth, view_channel=True, connect=True, speak=True)
+    msg2 = await ctx.send('The channel will delete when you leave')
+    await asyncio.sleep(10)
+    await msg.delete()
+    await msg2.delete()
 
 @formexe.command(pass_context=True , aliases=['ct', 'closet'])
 @commands.has_permissions(manage_messages=True)
-async def close(ctx):
-    channel = ctx.message.channel
-    if ('ticket') in channel.name:
+async def close(ctx, channel : discord.TextChannel = None):
+    channel = ctx.message.channel if not channel else channel
+    if ('ticket-logs') in channel.name:
+        pass
+    elif ('ticket') in channel.name:
+        await channel.delete()
+    elif ('report') in channel.name:
         await channel.delete()
     else:
         pass
+
+@formexe.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None:
+        pass
+    elif before.channel.name == (f'{member.name} ticket') and after.channel is None:
+            channel = formexe.get_channel(before.channel.id)
+            await channel.delete()
